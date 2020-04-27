1bc70cd59a11e8a9fe8ccc8aad9799c71656773f
mostafa-rasheed <mstafarasheed@gmail.com>
2020-04-28 02:59:28 +0300

    authentication2

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,13 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/','TaskController@index');
+Route::get('task/{id}', 'TaskController@show');
+Route::post('store','TaskController@store');
+Route::delete('delete/{id}','TaskController@destroy') ;
+Route::post('edit/{id}', 'TaskController@edit');
+Route::patch('update/{id}','TaskController@update');
+
+
+
+
